847c93f2317ed63dc97294770f6a1a075790a2ab
realrasengan <andysl@gmail.com>
2023-03-16 01:39:30 +0000

    AIQA nodeJS OpenAI QA

---
+++ README.md
@@ -0,0 +1,63 @@
+# AI QA (OpenAI)
+
+You can use this nodejs class to load a PDF, extract its text and get OpenAI Embeddings. Then, you can create a chatbot that
+can answer questions about the PDF.
+
+## Requirements
+1. nodejs / npm (like 19+)
+
+2. OpenAI API KEY
+
+## Installation
+1. Install the npm modules
+```
+npm install langchain chromadb @dqbd/tiktoken pdf-parse
+```
+2. Install chromadb
+```
+git clone https://github.com/chroma-core/chroma
+```
+3. Go in the chroma folder
+```
+cd chroma
+```
+4. Run docker build
+```
+docker-compose up -d --build
+```
+
+## What you get
+
+You get a class called `AIQA` which loads a PDF and can be asked questions with a `query` method. It can be used like this:
+
+```
+import AIQA from "./aiqa.js";
+
+const KEY = "sk-...";
+const CHUNK_SIZE = 800;
+const DOC = "example.pdf";
+
+(async function() {
+
+  let aiqa = await new AIQA(KEY, CHUNK_SIZE, DOC);
+  let history="";
+
+  let result = await aiqa.query("What do you know about this?",history);
+  console.dir(result.text);
+})();
+```
+
+## What can you use this for
+
+1. Chat Bot (Telegram, Slack, IRC, Webpage, etc.)
+
+2. ?
+
+## Copyright
+
+Copyright (c) 2023 Andrew Lee. (andrew@imperialfamily.com)
+
+All Rights Reserved.
+
+MIT Licensed.
+
---
+++ aiqa.js
@@ -0,0 +1,43 @@
+import { OpenAIEmbeddings } from "langchain/embeddings";
+import { Chroma } from "langchain/vectorstores";
+import { TokenTextSplitter } from "langchain/text_splitter";
+import { OpenAI } from "langchain/llms";
+import { ChatVectorDBQAChain } from "langchain/chains";
+import { PDFLoader } from "langchain/document_loaders";
+import { ChromaClient } from 'chromadb';
+const chroma_client = new ChromaClient();
+
+class AIQA {
+  #loader;
+  #docs;
+  #textSplitter;
+  #pdf;
+  #embeddings;
+  #vectorDB;
+
+  #qa;
+
+  constructor(KEY, CHUNK_SIZE, doc) {
+    return (async () => {
+      this.#loader = new PDFLoader(doc);
+      this.#docs = await this.#loader.load();
+      this.#textSplitter = new TokenTextSplitter({
+        chunkSize: CHUNK_SIZE,
+        chunkOverlap: 0,
+      });
+      this.#pdf = await this.#textSplitter.splitDocuments(this.#docs);
+      this.#embeddings = new OpenAIEmbeddings({openAIApiKey: KEY});
+      this.#vectorDB = await Chroma.fromDocuments(this.#pdf, this.#embeddings, {});
+      this.#qa = ChatVectorDBQAChain.fromLLM(new OpenAI({openAIApiKey: KEY, temperature:0, model_name:"gpt-3.5-turbo"}), this.#vectorDB, {returnSourceDocuments:true});
+      return this;
+    })();
+  }
+  query(q,h) {
+    return (async () => {
+      let result =  await this.#qa.call({"question": q, "chat_history":h});
+      result.text = result.text.trim();
+      return result;
+    })();
+  }
+}
+export default AIQA;
